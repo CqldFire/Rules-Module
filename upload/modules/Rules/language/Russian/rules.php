745689835a25149e7b02cea9bc21445237114b30
Coldfire <coldfiire1124@gmail.com>
2021-01-04 13:17:42 -0800

    Create rules.php
    Add Russian language

---
+++ upload/modules/Rules/language/Russian/rules.php
@@ -0,0 +1,52 @@
+<?php 
+/*
+ *	Rules module made by Coldfire
+ *	https://coldfiredzn.com
+ *
+ *	Using code from the vote module by Partydragen and Samerton
+ *
+ *  Russian translation made by LOyoujoLI
+ */
+
+$language = array( 
+	'rules_buttons' => 'Кнопки',
+	'rules_button_name' => 'Название кнопки',
+	'no_rules_buttons' => 'Нету готовых кнопок',
+	'rules_button_link' => 'Ссылка для этой кнопки',
+	'new_button' => 'Новая кнопка',
+	'edit_button' => 'Редактирование кнопки',
+	'delete_button' => 'Вы действительно хотите удалить эту кнопку?',
+	'button_created_successfully' => 'Кнопка добавлена.',
+	'button_updated_successfully' => 'Кнопка обновлена.',
+	'button_deleted_successfully' => 'Кнопка удалена.',
+	'button_name_required' => 'Напишите название этой кнопки.',
+	'button_link_required' => 'Придумайте ссылку для этой кнопки.',
+	'button_name_minimum' => 'Название кнопки должно иметь минимум 1 символ.',
+	'button_link_minimum' => 'Ссылка на кнопку должна иметь минимум 1 символ.',
+	'button_name_maximum' => 'Название кнопки может иметь максимум 96 символов.',
+	'button_link_maximum' => 'Ссылка на кнопку может иметь максимум 96 символов.',
+	'rules' => 'Правила',
+	'rules_catagories' => 'Категории',
+	'rules_catagory_name' => 'Название категории',
+	'no_rules_catagories' => 'Нету готовых к использованию категорий',
+	'rules_catagory_icon' => 'Иконка для категории (оставьте пустым для отключения)',
+	'rules_catagory_rules' => 'Правила',
+	'new_catagory' => 'Новая категория',
+	'edit_catagory' => 'Редактирование категории',
+	'delete_catagory' => 'Вы действительно хотите удалить эту категория?',
+	'message' => 'Текст для отображения на странице с правилами',
+	'message_maximum' => 'Произошла ошибка. Текст с правилами должен содержать максимум 2048 символов. Иконка может содердать максимум 64 символа.',
+	'link_location' => 'Расположение ссылки',
+	'icon' => 'Иконка',
+	'rules_action' => 'Действие',
+	'icon_example' => 'Пример: <i class="fas fa-gavel"></i>',
+	'catagory_created_successfully' => 'Категория добавлена.',
+	'catagory_updated_successfully' => 'Категория обновлена.',
+	'catagory_deleted_successfully' => 'Категория удалена.',
+	'catagory_name_required' => 'Напишите название этой категории.',
+	'catagory_rules_required' => 'Требуется готовый список правил.',
+	'catagory_name_minimum' => 'Название категории должно иметь минимум 1 символ.',
+	'catagory_rules_minimum' => 'Список правил должен содержать как минимум 1 символ.',
+	'catagory_name_maximum' => 'Название категории может иметь максимум 96 символов.',
+	'catagory_icon_maximum' => 'Иконка категории может иметь максимум of 96 символов.'
+);
