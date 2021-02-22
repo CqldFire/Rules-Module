64340058269b9e45b4e70ff150b6bb1ea0a3cc2c
Coldfire <coldfiire1124@gmail.com>
2021-02-22 08:21:37 -0800

    Change version number

--- upload/modules/Rules/module.php
+++ upload/modules/Rules/module.php
@@ -14,7 +14,7 @@ class Rules_Module extends Module {
 		
 		$name = 'Rules';
 		$author = '<a href="https://coldfiredzn.com" target="_blank" rel="nofollow noopener">Coldfire</a>';
-		$module_version = '1.5.8';
+		$module_version = '1.6.0';
 		$nameless_version = '2.0.0-pr9';
 		
 		parent::__construct($this, $name, $author, $module_version, $nameless_version);
