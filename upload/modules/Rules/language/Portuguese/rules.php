45b98247cf82cb94257a9ac7a165760a7c49474b
Coldfire <coldfiire1124@gmail.com>
2022-02-26 13:55:51 -0800

    Portuguese translation (thanks Lightxx)

---
+++ upload/modules/Rules/language/Portuguese/rules.php
@@ -0,0 +1,50 @@
+<?php 
+/*
+ *    Rules module made by Coldfire
+ *    https://coldfiredzn.com
+ *
+ *    Using code from the vote module by Partydragen and Samerton
+ */
+
+$language = array( 
+    'rules_buttons' => 'Butões',
+    'rules_button_name' => 'Nome do botão',
+    'no_rules_buttons' => 'Sem botões definidos',
+    'rules_button_link' => 'Link do Botão',
+    'new_button' => 'Novo Botão',
+    'edit_button' => 'Editar botão',
+    'delete_button' => 'Quer mesmo deletar este botão?',
+    'button_created_successfully' => 'Botão criado com sucesso.',
+    'button_updated_successfully' => 'Botão atualizado com sucesso.',
+    'button_deleted_successfully' => 'Botão deletado com sucesso.',
+    'button_name_required' => 'É necessário um nome para o botão.',
+    'button_link_required' => 'É necessário um link para o botão.',
+    'button_name_minimum' => 'O nome do botão deve ter no minimo 1 caracter.',
+    'button_link_minimum' => 'O link do botão deve ter no minimo 1 caracter.',
+    'button_name_maximum' => 'O nome do botão deve ter no máximo 96 caracter.',
+    'button_link_maximum' => 'O link do botão deve ter no máximo 96 caracter.',
+    'rules' => 'Regras',
+    'rules_catagories' => 'Catagorias',
+    'rules_catagory_name' => 'Nome da Categoria',
+    'no_rules_catagories' => 'Sem categorias definidas',
+    'rules_catagory_icon' => 'Icone da categoria (deixe em branco se não quiser icone)',
+    'rules_catagory_rules' => 'Regras',
+    'new_catagory' => 'Nova Categoria',
+    'edit_catagory' => 'Editar Catagoria',
+    'delete_catagory' => 'Quer mesmo deletar esta categoria?',
+    'message' => 'Texto para mostrar na página inicial das regras',
+    'message_maximum' => 'Ocurreu um erro. Esta mensagem só pode ter no máximo 2048 caracteres. O icone só pode ter 64 caracteres.',
+    'link_location' => 'Localização do link',
+    'icon' => 'Icone',
+    'rules_action' => 'Ação',
+    'icon_example' => 'Exemplo: <i class="fas fa-gavel"></i>',
+    'catagory_created_successfully' => 'Catagoria criada com sucesso.',
+    'catagory_updated_successfully' => 'Catagoria atualizada com sucesso.',
+    'catagory_deleted_successfully' => 'Catagoria deletada com sucesso.',
+    'catagory_name_required' => 'É necessário um nome para a categoria.',
+    'catagory_rules_required' => 'Uma lista de regras é necessária.',
+    'catagory_name_minimum' => 'O nome da categoria deve ter no minimo 1 caracter.',
+    'catagory_rules_minimum' => 'A lista de regras deve ter no minimo 1 caracter.',
+    'catagory_name_maximum' => 'O nome da categoria deve ter no máximo 96 caracter.',
+    'catagory_icon_maximum' => 'O icone da categoria deve ter no máximo 96 caracter.'
+);
