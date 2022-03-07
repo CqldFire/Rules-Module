233986cf577f11ffe84404de132b1ff20c604928
Coldfire <coldfiire1124@gmail.com>
2022-03-06 17:24:20 -0800

    Add Romanian translation (thanks Horatiu)

---
+++ upload/modules/Rules/language/Romanian/rules.php
@@ -0,0 +1,50 @@
+<?php 
+/*
+ *	Rules module made by Coldfire
+ *	https://coldfiredzn.com
+ *
+ *	Using code from the vote module by Partydragen and Samerton
+ */
+
+$language = array( 
+	'rules_buttons' => 'Butoane',
+	'rules_button_name' => 'Numele Butonului',
+	'no_rules_buttons' => 'Nu s-au găsit butoane',
+	'rules_button_link' => 'Link Buton',
+	'new_button' => 'Buton Nou',
+	'edit_button' => 'Editezi Butonul',
+	'delete_button' => 'Ești sigur că dorești să ștergi acest buton?',
+	'button_created_successfully' => 'Butonul a fost creat cu succes.',
+	'button_updated_successfully' => 'Butonul a fost updatat cu succes.',
+	'button_deleted_successfully' => 'Butonul a fost șters cu succes.',
+	'button_name_required' => 'Este necesar să introduceți numele butonului.',
+	'button_link_required' => 'Este necesar să introduceți un link butonului.',
+	'button_name_minimum' => 'Numele butonului trebuie să aibă minimum 1 caracter.',
+	'button_link_minimum' => 'Link-ul butonului trebuie să aibă minimum 1 caractere.',
+	'button_name_maximum' => 'Numele butonului trebuie să aibă maximum 96 caractere.',
+	'button_link_maximum' => 'Link-ul butonului trebuie să aibă maximum 96 caractere.',
+	'rules' => 'Reguli',
+	'rules_catagories' => 'Categorii',
+	'rules_catagory_name' => 'Numele Categoriei',
+	'no_rules_catagories' => 'Nu s-au găsit categorii.',
+	'rules_catagory_icon' => 'Iconiță categorie (lăsați liber pentru dezactivare)',
+	'rules_catagory_rules' => 'Reguli',
+	'new_catagory' => 'Categorie Nouă',
+	'edit_catagory' => 'Editezi Categoria',
+	'delete_catagory' => 'Ești sigur că dorești să ștergi această categorie?',
+	'message' => 'Text de afișat în fila de pornire a paginii de reguli.',
+	'message_maximum' => 'A aparut o eroare. Mesajul privind regulile este obligatoriu și trebuie să aibă maximum 2048 de caractere. Pictograma trebuie să aibă maximum 64 de caractere.',
+	'link_location' => 'Locație Link',
+	'icon' => 'Iconiță',
+	'rules_action' => 'Acțiune',
+	'icon_example' => 'Exemplu: <i class="fas fa-gavel"></i>',
+	'catagory_created_successfully' => 'Categoria a fost creată cu succes.',
+	'catagory_updated_successfully' => 'Categoria a fost updatată cu succes.',
+	'catagory_deleted_successfully' => 'Categoria a fost ștearsă cu succes.',
+	'catagory_name_required' => 'Numele categoriei este obligatoriu.',
+	'catagory_rules_required' => 'O listă de reguli este obligatorie.',
+	'catagory_name_minimum' => 'Numele categoriei trebuie să aibă minimum 1 caracter.',
+	'catagory_rules_minimum' => 'Lista regulilor trebuie să aibă minimum 1 caracter.',
+	'catagory_name_maximum' => 'Numele categoriei trebuie să aibă maximum 96 caractere.',
+	'catagory_icon_maximum' => 'Iconița categoriei trebuie să aibă maximum 96 caractere.'
+);
